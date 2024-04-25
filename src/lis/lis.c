823c2fdea773b81f59711f6d6c2d137512f10dcf
spenning <whimizera@mailbox.org>
2024-04-25 16:27:50 +0200

    fixed valgrind issues in lis and lists

--- src/lis/lis.c
+++ src/lis/lis.c
@@ -6,11 +6,39 @@
 /*   By: spenning <spenning@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 14:32:59 by spenning          #+#    #+#             */
-/*   Updated: 2024/04/25 14:38:21 by spenning         ###   ########.fr       */
+/*   Updated: 2024/04/25 15:55:08 by spenning         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/push_swap.h"
+
+// int	len_is(int *stack_c, int len)
+// {
+// 	int	index;
+// 	int	inner_index;
+// 	int	value;
+// 	int size;
+
+// 	size = 0;
+// 	index = len;
+// 	inner_index = index;
+// 	while (index >= 0)
+// 	{
+// 		value = stack_c[index];
+// 		while (inner_index <= len)
+// 		{
+// 			if (stack_c[inner_index] > value)
+// 			{
+// 				value = stack_c[inner_index];
+// 				size += 1;
+// 			}
+// 			inner_index++;
+// 		}
+// 		index--;
+// 		inner_index = index;
+// 	}
+// 	return (size);
+// }
 
 void	is(int *stack_c, int *lis, int len)
 {
@@ -42,7 +70,7 @@ void	init_lis_stack(t_pslist **stack_a)
 	t_pslist	*temp;
 	int			val;
 
-	temp = *stack_a;
+	temp = (*stack_a);
 	temp->lis = 1;
 	val = temp->content;
 	while (temp != NULL)
@@ -67,7 +95,7 @@ void	init_lis(t_pslist **stack_a, int *lis, int len)
 	index = 0;
 	max = lis[0];
 	index_of_max = 0;
-	while (index <= len)
+	while (index < len)
 	{
 		if (lis[index] > max)
 		{
@@ -91,10 +119,10 @@ int	lis(t_pslist**stack_a)
 	int	len;
 
 	len = ps_lstsize((*stack_a)) - 1;
-	lis = malloc(sizeof(int) * len);
+	lis = ft_calloc(sizeof(int), len);
 	if (lis == NULL)
 		return (EXIT_FAILURE);
-	stack_c = malloc(sizeof(int) * ps_lstsize((*stack_a)));
+	stack_c = ft_calloc(sizeof(int), ps_lstsize((*stack_a)));
 	init_c(stack_a, stack_c);
 	is(stack_c, lis, len);
 	free(stack_c);
--- src/lists/list.c
+++ src/lists/list.c
@@ -4,7 +4,7 @@ t_pslist	*ps_lstnew(int content)
 {
 	t_pslist	*new_node;
 
-	new_node = malloc(sizeof(t_pslist));
+	new_node = ft_calloc(sizeof(t_pslist), 1);
 	if (new_node == NULL)
 		return (NULL);
 	new_node->content = content;
