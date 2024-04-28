12cf70cf472d281a9295d0f6b6a276a6aefd7c66
spenning <whimizera@mailbox.org>
2024-04-28 16:35:30 +0200

    bubble_sort norm proof

--- src/algorithms/bubble_sort.c
+++ src/algorithms/bubble_sort.c
@@ -1,29 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bubble_sort.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spenning <spenning@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/28 16:33:00 by spenning          #+#    #+#             */
+/*   Updated: 2024/04/28 16:35:06 by spenning         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../inc/push_swap.h"
 
-void bubble_sort(t_pslist	**stack_a, t_pslist	**stack_b)
+void	bubble_sort(t_pslist	**stack_a, t_pslist	**stack_b)
 {
 	int	len;
 
 	len = ps_lstsize((*stack_a));
 	while (!check_n_sorted(stack_a, len, '+'))
 	{
-		if((*stack_b) == NULL || (*stack_a)-> content > (*stack_b)->content)
-		{
+		if ((*stack_b) == NULL || (*stack_a)-> content > (*stack_b)->content)
 			push(stack_a, stack_b, pa);
-			// print_stack(stack_a, 'a');
-			// print_stack(stack_b, 'b');
-		}
-		if((*stack_a)-> content < (*stack_b)->content)
+		if ((*stack_a)-> content < (*stack_b)->content)
 		{
 			push(stack_b, stack_a, pb);
-			// print_stack(stack_a, 'a');
-			// print_stack(stack_b, 'b');
 			swap(stack_a, sa);
-			// print_stack(stack_a, 'a');
-			// print_stack(stack_b, 'b');
 		}
-		// if(check_n_sorted(stack_a, ps_lstsize((*stack_a)), '+') && check_n_sorted(stack_b, ps_lstsize((*stack_b)), '-'))
-		if(check_sorted_stack(stack_a, stack_b))
+		if (check_sorted_stack(stack_a, stack_b))
 		{
 			while ((*stack_b) != NULL)
 				push(stack_b, stack_a, pb);
