d08c2b13f087771914090c467dba2c68efc130a2
spenning <whimizera@mailbox.org>
2024-04-28 18:08:49 +0200

    checker norminette proof

--- src/checker/checker.c
+++ src/checker/checker.c
@@ -1,23 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spenning <spenning@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/28 18:06:00 by spenning          #+#    #+#             */
+/*   Updated: 2024/04/28 18:08:28 by spenning         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../inc/push_swap.h"
 
 // maybe also accept + in program, not sure 
 // it is mandatory
-int check_stack_argument_val(char *test)
+int	check_stack_argument_val(char *test)
 {
 	int	index;
 
 	index = 0;
 	if (test[index] == '-')
 		index++;
-	while(test[index] != '\0')
+	while (test[index] != '\0')
 	{
-		if(!ft_isdigit(test[index++]))
+		if (!ft_isdigit(test[index++]))
 			return (EXIT_FAILURE);
 	}
 	return (EXIT_SUCCESS);
 }
 
-int check_stack_argument_int(char *test)
+int	check_stack_argument_int(char *test)
 {
 	char	*cmp;
 	int		ret;
@@ -38,60 +50,60 @@ int check_stack_argument_int(char *test)
 	return (EXIT_SUCCESS);
 }
 
-int check_stack_arguments(char **argv)
+int	check_stack_arguments(char **argv)
 {
 	int	index;
 
 	index = 0;
-	while(argv[index] != NULL)
+	while (argv[index] != NULL)
 	{
-		if(check_stack_argument_val(argv[index]))
-			return(EXIT_FAILURE);
-		if(check_stack_argument_int(argv[index++]))
-			return(EXIT_FAILURE);
+		if (check_stack_argument_val(argv[index]))
+			return (EXIT_FAILURE);
+		if (check_stack_argument_int(argv[index++]))
+			return (EXIT_FAILURE);
 	}
-	return(EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
 
-int check_stack_duplicate(char **argv)
+int	check_stack_duplicate(char **argv)
 {
-	int	oi;
-	int	ii;
-	size_t len;
+	int		oi;
+	int		ii;
+	size_t	len;
 
 	len = 0;
 	oi = 0;
 	ii = 1;
-	while(argv[oi] != NULL)
+	while (argv[oi] != NULL)
 	{
-		while(argv[ii] != NULL)
+		while (argv[ii] != NULL)
 		{
 			len = ft_strlen(argv[oi]);
-			if(len < ft_strlen(argv[ii]))
+			if (len < ft_strlen(argv[ii]))
 				len = ft_strlen(argv[ii]);
-			if(!ft_strncmp(argv[oi], argv[ii++], len))
+			if (!ft_strncmp(argv[oi], argv[ii++], len))
 			{
-				ft_printf("fail: argv[oi]%s argv[ii]%s\n", argv[oi], argv[--ii]);
-				return(EXIT_FAILURE);
+				ft_printf("fail: argv-oi%s argv-ii%s\n", argv[oi], argv[--ii]);
+				return (EXIT_FAILURE);
 			}
 		}
 		oi++;
 		ii = oi + 1;
 	}
-	return(EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
 
-int check_stack(char **argv)
+int	check_stack(char **argv)
 {
 	if (check_stack_arguments(argv))
 	{
 		ft_printf("fail arg\n");
-		return(1);
+		return (1);
 	}
 	if (check_stack_duplicate(argv))
 	{
 		ft_printf("fail dup\n");
-		return(1);
+		return (1);
 	}
-	return(0);
+	return (0);
 }
