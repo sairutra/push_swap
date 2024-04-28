3468f4391a38f3797a546b27554f0c2f280cb33d
spenning <whimizera@mailbox.org>
2024-04-28 18:11:44 +0200

    list operations norm proof

--- src/lists/list_add.c
+++ src/lists/list_add.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   list_add.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spenning <spenning@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/28 18:09:23 by spenning          #+#    #+#             */
+/*   Updated: 2024/04/28 18:10:20 by spenning         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../inc/push_swap.h"
 
 void	ps_lstadd_front(t_pslist	**lst, t_pslist	*new)
@@ -16,4 +28,3 @@ void	ps_lstadd_back(t_pslist	**lst, t_pslist	*new)
 	else
 		last->next = new;
 }
-
--- src/lists/list_size.c
+++ src/lists/list_size.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   list_size.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spenning <spenning@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/28 18:11:10 by spenning          #+#    #+#             */
+/*   Updated: 2024/04/28 18:11:19 by spenning         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../inc/push_swap.h"
 
 int	ps_lstsize(t_pslist *lst)
 {
-	int		size;
+	int			size;
 	t_pslist	*temp;
 
 	size = 0;
@@ -14,4 +26,3 @@ int	ps_lstsize(t_pslist *lst)
 	}
 	return (size);
 }
-
