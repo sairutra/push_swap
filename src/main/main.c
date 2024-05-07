8971c5b9b5907d1697f7dd0fef3bb2e20c188261
spenning <whimizera@mailbox.org>
2024-05-07 18:09:35 +0200

    added check sorting stack in main

--- src/main/main.c
+++ src/main/main.c
@@ -6,7 +6,7 @@
 /*   By: spenning <spenning@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/04/25 13:15:31 by spenning      #+#    #+#                 */
-/*   Updated: 2024/05/07 18:00:46 by spenning      ########   odam.nl         */
+/*   Updated: 2024/05/07 18:07:41 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,29 @@ void	free_all(char **argv, t_pslist	*stack_a, t_pslist	*stack_b)
 	ps_lstclear(&stack_b);
 }
 
+
+void	check_stack_sorted(t_pslist **stacka, char **argv)
+{
+	if(check_n_sorted(stacka, ps_lstsize((*stacka)), '+'))
+	{
+		ps_lstclear(stacka);
+		free_char_array(argv);
+		exit(EXIT_SUCCESS);
+	}
+}
+
 void	check_args(char **argv)
 {
 	if (argv[0] == NULL)
 	{
 		ft_putstr_fd("Error\n", 2);
-		exit(EXIT_SUCCESS);
+		exit(EXIT_FAILURE);
 	}
 	if (check_stack(argv))
 	{
 		free_char_array(argv);
 		ft_putstr_fd("Error\n", 2);
-		exit(EXIT_SUCCESS);
+		exit(EXIT_FAILURE);
 	}
 }
 
@@ -47,6 +58,7 @@ int	main(int argc, char **argv)
 	check_args(argv);
 	if (create_stack(&stack_a, argv))
 		exit(EXIT_FAILURE);
+	check_stack_sorted(&stack_a, argv);
 	sort_index(&stack_a);
 	add_binary(&stack_a);
 	if (ps_lstsize(stack_a) <= 5)
